d6e3751b2483b2050d7deebeac3b61cc7bb06eef
Agu Chux <agu.chux@yahoo.com>
2023-01-08 13:43:24 +0100

    Added workspace.json

--- libs/metrics-models/src/lib/index.ts
+++ libs/metrics-models/src/lib/index.ts
@@ -1,4 +1,3 @@
-
 import mongoose from 'mongoose';
 
 import Schools from './schools.model';
@@ -7,14 +6,14 @@ import Connections from './connections.model';
 import Owners from './owners.model';
 import Roles from './roles.model';
 
-const { NEXT_PUBLIC_MONGOOSE_URI } = process.env ;
+const { NEXT_PUBLIC_MONGOOSE_URI } = process.env;
 
 export const dbCon = async () => {
   const conn = await mongoose
     .connect(NEXT_PUBLIC_MONGOOSE_URI as string)
     .then(() => {})
     .catch((err) => console.log(err));
-  console.log("Mongoose Connection Established");
+  console.log('Mongoose Connection Established');
   return {
     conn,
     Schools,
