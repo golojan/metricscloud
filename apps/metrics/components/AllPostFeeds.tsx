7f46a5e32200ca520f1eff30cb9201cf5b415349
Agu Chux <agu.chux@yahoo.com>
2023-01-09 13:19:18 +0100

    Metrics Owner Added 34

--- apps/metrics/components/AllPostFeeds.tsx
+++ apps/metrics/components/AllPostFeeds.tsx
@@ -1,7 +1,7 @@
 import React from "react";
 import PostFeed from "./PostFeed";
 import { useAtom } from "jotai";
-import { profileAtom } from '@metrics/store';
+import { profileAtom } from '@metricsai/metrics-store';
 
 function AllPostFeeds() {
   const [profile] = useAtom(profileAtom);
