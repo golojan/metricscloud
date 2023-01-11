b8f9a91966984fd538dd41eadd417144456dd06e
Agu Chux <agu.chux@yahoo.com>
2023-01-11 04:38:07 +0100

    Fix Profile 10

--- apps/metricsapp/pages/_app.tsx
+++ apps/metricsapp/pages/_app.tsx
@@ -2,7 +2,6 @@ import React, { useEffect } from 'react';
 import type { AppProps } from 'next/app';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import 'bootstrap-select/dist/css/bootstrap-select.min.css';
-import '../public/DataTables/datatables.min.css';
 import '../styles/globals.scss';
 import { Provider } from 'react-redux';
 import { Provider as JotaiProvider } from 'jotai';
