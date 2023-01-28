04b7b3b775f46828d03032c26ca10c0349d1ce78
Agu Chux <agu.chux@yahoo.com>
2023-01-28 18:21:06 +0100

    CSS Done

--- apps/metricsapi/pages/index.tsx
+++ apps/metricsapi/pages/index.tsx
@@ -2,7 +2,7 @@ export function Index() {
   return (
     <>
       <div className="relative w-screen h-screen bg-black">
-        <h1 className="absolute top-[50%] left-[50%] text-white h1">api.metrics.ng</h1>
+        <h1 className="absolute top-[50%] w-[50%] mx-auto text-white text-xxl">api.metrics.ng</h1>
       </div>
     </>
   );
