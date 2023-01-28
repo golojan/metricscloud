22886dbcdcb82f828a47883c883c1143f3618bd6
Agu Chux <agu.chux@yahoo.com>
2023-01-28 17:14:50 +0100

    API 6

--- apps/metricsapi/next.config.js
+++ apps/metricsapi/next.config.js
@@ -7,12 +7,13 @@ const { withNx } = require('@nrwl/next/plugins/with-nx');
  * @type {import('@nrwl/next/plugins/with-nx').WithNxOptions}
  **/
 const nextConfig = {
+  target: 'server',
+  reactStrictMode: true,
   nx: {
     // Set this to true if you would like to to use SVGR
     // See: https://github.com/gregberge/svgr
     svgr: false,
   },
-  useFileSystemPublicRoutes: false,
 };
 
 module.exports = withNx(nextConfig);
