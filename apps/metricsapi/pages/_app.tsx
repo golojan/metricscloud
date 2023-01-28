1346b805bf2e11dc6e7a6bfc797f965ccfc61e32
Agu Chux <agu.chux@yahoo.com>
2023-01-28 18:12:23 +0100

    CSS Done

--- apps/metricsapi/pages/_app.tsx
+++ apps/metricsapi/pages/_app.tsx
@@ -1,5 +1,6 @@
 import { AppProps } from 'next/app';
 import Head from 'next/head';
+import '../styles/globals.css'; // Global CSS
 function CustomApp({ Component, pageProps }: AppProps) {
   return (
     <>
