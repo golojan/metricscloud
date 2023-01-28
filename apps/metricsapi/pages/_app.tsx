65c98e1c1f1f95fcfd66cfeccb0cb14ac27eda01
Agu Chux <agu.chux@yahoo.com>
2023-01-28 17:46:01 +0100

    cleanup

--- apps/metricsapi/pages/_app.tsx
+++ apps/metricsapi/pages/_app.tsx
@@ -1,16 +1,12 @@
 import { AppProps } from 'next/app';
 import Head from 'next/head';
-import './styles.css';
-
 function CustomApp({ Component, pageProps }: AppProps) {
   return (
     <>
       <Head>
-        <title>Welcome to metricsapi!</title>
+        <title>MetricsAI.API</title>
       </Head>
-      <main className="app">
-        <Component {...pageProps} />
-      </main>
+      <Component {...pageProps} />
     </>
   );
 }
