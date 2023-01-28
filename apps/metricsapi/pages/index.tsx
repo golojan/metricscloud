c3a93f69ce095079d69307283511c12f39a1b4f8
Agu Chux <agu.chux@yahoo.com>
2023-01-28 18:08:30 +0100

    Server 1

--- apps/metricsapi/pages/index.tsx
+++ apps/metricsapi/pages/index.tsx
@@ -1,6 +1,10 @@
 export function Index() {
   return (
-    <>[]</>
+    <>
+      <div className="relative w-screen h-screen bg-blend-darken">
+        <h1 className="absolute top-[40%] left-[40%] text-white">api.metrics.ng</h1>
+      </div>
+    </>
   );
 }
 
