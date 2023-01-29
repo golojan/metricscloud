7e28a9a4034736d86e9d11cd883c93fc67e5dae4
Agu Chux <agu.chux@yahoo.com>
2023-01-29 15:19:21 +0100

    Server 2

--- apps/metricsapi/next.config.js
+++ apps/metricsapi/next.config.js
@@ -10,7 +10,6 @@ const nextConfig = {
   reactStrictMode: true,
   // This is required for the serverless target
   // See: https://nextjs.org/docs/messages/serverless-target-node14
-  target: 'serverless',
   nx: {
     // Set this to true if you would like to to use SVGR
     // See: https://github.com/gregberge/svgr
