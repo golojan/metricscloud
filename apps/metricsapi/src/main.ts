27d819e00e403774cc7eb2c6c257dda80f65be7b
Agu Chux <agu.chux@yahoo.com>
2023-01-30 16:09:21 +0100

    API issues

--- apps/metricsapi/src/main.ts
+++ apps/metricsapi/src/main.ts
@@ -1,5 +1,9 @@
 import express from 'express';
 const app = express();
+const path = require('path');
+
+//setup static files
+app.use(express.static(path.join(__dirname, 'public')));
 
 app.get('/', (req, res) => {
   res.send({ message: 'Hello API' });
