3966f81c41b459f24c3930173695b45d11f9c9e5
Agu Chux <agu.chux@yahoo.com>
2023-02-05 13:47:49 +0100

    Server fix 3

--- apps/metricsapi/models/index.ts
+++ apps/metricsapi/models/index.ts
@@ -23,8 +23,7 @@ const { MONGOOSE_URI } = process.env;
 export const allowCors = (fn) => async (req, res) => {
   res.setHeader('Access-Control-Allow-Credentials', true);
   res.setHeader('Access-Control-Allow-Origin', '*');
-  res.setHeader('Access-Control-Allow-Origin', req.headers.origin);
-  res.setHeader('Access-Control-Allow-Methods', 'GET,OPTIONS,PATCH,DELETE,POST,PUT');
+  res.setHeader('Access-Control-Allow-Methods', 'GET,POST');
   res.setHeader(
     'Access-Control-Allow-Headers',
     'X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version',
--- apps/metricsapi/pages/api/schools/domains/[domain]/index.ts
+++ apps/metricsapi/pages/api/schools/domains/[domain]/index.ts
@@ -1,13 +1,13 @@
 // Next.js API route support: https://nextjs.org/docs/api-routes/introduction
 import type { NextApiRequest, NextApiResponse } from "next";
+import { dbCon, allowCors } from '../../../../../models';
 
 type Data = {
   error?: string;
 };
 
-export default function handler(
-  req: NextApiRequest,
-  res: NextApiResponse<Data>
-) {
-  res.status(400).json({ error: "Invalid API Route" });
-}
+const handler = (req: NextApiRequest, res: NextApiResponse<Data>) => {
+  res.status(400).json({ error: 'Invalid API Route' });
+};
+
+export default allowCors(handler); 
