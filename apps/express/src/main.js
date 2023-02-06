5ff53a79d721f6937a11d435b492f5ac1865cf4a
Agu Chux <agu.chux@yahoo.com>
2023-02-06 12:50:02 +0100

    Server 2

---
+++ apps/express/src/main.js
@@ -0,0 +1,12 @@
+'use strict';
+exports.__esModule = true;
+var express_1 = require('express');
+var app = (0, express_1['default'])();
+
+app.get('/', function (req, res) {
+  res.send({ message: 'Hello API' });
+});
+
+app.listen(3000, function () {
+  console.log('API is running on port 3000');
+});
