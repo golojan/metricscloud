369d3b28d644ba492c5495ed3f2c652bf0372b0f
Agu Chux <agu.chux@yahoo.com>
2023-02-06 12:11:30 +0100

    Server 2

---
+++ apps/express/src/main.ts
@@ -0,0 +1,10 @@
+import express from 'express';
+const app = express();
+
+app.get('/', (req, res) => {
+  res.send({ message: 'Hello API' });
+});
+
+app.listen(3000, () => {
+  // Server is running
+});
--- apps/metricsapi/models/index.ts
+++ apps/metricsapi/models/index.ts
@@ -21,6 +21,7 @@ import MRCs from './mrcs.model';
 const { MONGOOSE_URI } = process.env;
 
 export const allowCors = (fn) => async (req, res) => {
+  res.header('Content-Type', 'application/json');
   res.setHeader('Access-Control-Allow-Credentials', true);
   res.setHeader('Access-Control-Allow-Origin', '*');
   res.setHeader('Access-Control-Allow-Methods', 'GET,POST');
@@ -28,7 +29,6 @@ export const allowCors = (fn) => async (req, res) => {
     'Access-Control-Allow-Headers',
     'X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version',
   );
-  res.header('Content-Type', 'application/json');
   if (req.method === 'GET' || req.method === 'POST') {
     return await fn(req, res);
   } else {
